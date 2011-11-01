5e3a8bd615937e2e32a7c818a29474c27a74de3e
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-01 18:25:19 -0500

    Improve documentation of DistanceMetric interface.

--- src/cs437/som/DistanceMetric.java
+++ src/cs437/som/DistanceMetric.java
@@ -1,12 +1,20 @@
 package cs437.som;
 
 /**
- * Distance strategy for self-organizing maps.
+ * Distance strategy interface for self-organizing maps.
+ *
+ * Implementations of this interface are intended to define a method of
+ * determining the distance between 2 vectors.  Once an object of an
+ * implementing class is given to an SOM, that SOM assumes control of that
+ * object.  It should not be given to multiple SOMs or modified once it has
+ * been handed to an SOM.
+ * 
  */
 public interface DistanceMetric {
 
     /**
      * Calculate the distance between 2 vectors.
+     * 
      * @param v0 The first vector.
      * @param v1 The second vector.
      * @return The distance between v0 and v1.
