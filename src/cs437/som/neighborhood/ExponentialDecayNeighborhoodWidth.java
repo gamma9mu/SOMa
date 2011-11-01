0bf43b49c84fd568d23254fd341b689c160c15f6
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-01 17:43:21 -0500

    Add formula to exp. decay NW class.

--- src/cs437/som/neighborhood/ExponentialDecayNeighborhoodWidth.java
+++ src/cs437/som/neighborhood/ExponentialDecayNeighborhoodWidth.java
@@ -3,6 +3,14 @@ package cs437.som.neighborhood;
 /**
  * Neighborhood width strategy for self-organizing maps that decays the width
  * exponentially as the iterations progress.
+ *
+ * The exact behavior follows the formula:
+ *      w_i * e^(-t / t_max)
+ *  where
+ *      w_i   is the initial width of the neighborhood
+ *      e     is the base of the natural logarithm
+ *      t     is the current iteration
+ *      t_max is the maximum expected iteration
  */
 public class ExponentialDecayNeighborhoodWidth {
     private final double initialNeighborhoodWidth;
