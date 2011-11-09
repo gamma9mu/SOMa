3a6546184ad62545f4fa87ee9fbdd37be1780915
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-09 00:39:06 -0600

    Doc fixes: HyperbolicNeighborhoodWidthFunction.

--- src/cs437/som/neighborhood/HyperbolicNeighborhoodWidthFunction.java
+++ src/cs437/som/neighborhood/HyperbolicNeighborhoodWidthFunction.java
@@ -7,33 +7,24 @@ import cs437.som.NeighborhoodWidthFunction;
  *
  * <pre>
  * The exact behavior follows the formula:
- *      w_i / (t + t_max)
+ *      \frac{w_i}{(t + t_{max})}
  *  where
- *      w_i   is the initial width of the neighborhood
- *      t     is the current iteration
- *      t_max is the maximum expected iteration
+ *      w_i     is the initial width of the neighborhood
+ *      t       is the current iteration
+ *      t_{max} is the maximum expected iteration
  * </pre>
  */
 public class HyperbolicNeighborhoodWidthFunction implements NeighborhoodWidthFunction {
     private double expectedIterations = 0.0;
 
-    /**
-     * {@inheritDoc}
-     */
     public void setExpectedIterations(int expectedIterations) {
         this.expectedIterations = expectedIterations;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     public double neighborhoodWidth(int iteration) {
         return expectedIterations / (expectedIterations + iteration);
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public String toString() {
         return "HyperbolicNeighborhoodWidthFunction";
