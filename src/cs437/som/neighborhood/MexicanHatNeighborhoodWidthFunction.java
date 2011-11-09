c516a4e803b61ae24001dc659989bf1fc7625629
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-09 00:43:08 -0600

    Doc fixes: MexicanHatNeighborhoodWidthFunction.

--- src/cs437/som/neighborhood/MexicanHatNeighborhoodWidthFunction.java
+++ src/cs437/som/neighborhood/MexicanHatNeighborhoodWidthFunction.java
@@ -11,7 +11,7 @@ import cs437.som.NeighborhoodWidthFunction;
  * <pre>
  * The exact behavior follows the formula:
  *      \frac{2}{\sqrt{3 \sigma} \pi^{\frac{1}{4}}}
- *      e^{\frac{-t^2}{2 \sigma^2}} (1 - \frac{t^2}{\sigma^2})
+ *      \cdot e^{\frac{-t^2}{2 \sigma^2}} \cdot (1 - \frac{t^2}{\sigma^2})
  *  where
  *      \sigma  is the standard deviation of the corresponding Gaussian
  *      e       is the base of the natural logarithm
@@ -34,15 +34,9 @@ public class MexicanHatNeighborhoodWidthFunction implements NeighborhoodWidthFun
         variance = standardDeviation * standardDeviation;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     public void setExpectedIterations(int expectedIterations) {
     }
 
-    /**
-     * {@inheritDoc}
-     */
     public double neighborhoodWidth(int iteration) {
         double i2 = (double) iteration * iteration;
         return coefficient *
@@ -50,9 +44,6 @@ public class MexicanHatNeighborhoodWidthFunction implements NeighborhoodWidthFun
                 (1 - (i2 / variance));
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public String toString() {
         return "MexicanHatNeighborhoodWidthFunction";
