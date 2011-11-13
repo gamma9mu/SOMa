c5f8bf80a8d88464d2070302b999160d222b9b61
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-13 15:33:32 +0800

    Add features to EdgeDetectionRunner.

--- src/cs437/som/demo/EdgeDetectionRunner.java
+++ src/cs437/som/demo/EdgeDetectionRunner.java
@@ -1,40 +1,76 @@
 package cs437.som.demo;
 
-import javax.swing.JLabel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextField;
+import cs437.som.SOMBuilberConfigPanel;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * A form for running multiple EdgeDetectors and comparing their output to a
  * reference image.
  */
 public class EdgeDetectionRunner {
+    private static final long serialVersionUID = 0L;
+    
     private JRadioButton exhaustiveRadioButton;
     private JRadioButton sampledRadioButton;
-    private JTextField neuronCountInput;
     private JTextField iterationCountInput;
-    private JLabel referenceImage;
-    private JLabel outputImage;
-    private JLabel neu;
+    private SOMBuilberConfigPanel mapConfig;
+    private JComboBox outputImageCmb;
+    private JButton trainButton;
+    private JButton runButton;
+    private JPanel edgeDetectionRunnerForm;
+    private ImageFrame reference;
+    private ImageFrame output;
 
-    private int neuronCount;
+    public EdgeDetectionRunner() {
 
-    public int getIterations() {
-        return iterations;
+        iterationCountInput.setEnabled(false);
+        exhaustiveRadioButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (exhaustiveRadioButton.isSelected())
+                    iterationCountInput.setEnabled(false);
+            }
+        });
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(edgeDetectionRunnerForm);
+        frame.pack();
+        frame.setVisible(true);
+        trainButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                trainMap();
+            }
+        });
+        runButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                runMap();
+            }
+        });
+        outputImageCmb.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (outputImageCmb.getSelectedIndex() == 0) {
+                }
+            }
+        });
     }
 
-    public void setIterations(int iterations) {
-        this.iterations = iterations;
+    private void trainMap() {
+        ;
     }
 
-    private int iterations;
-
-    public int getNeuronCount() {
-        return neuronCount;
+    private void runMap() {
+        ;
     }
 
-    public void setNeuronCount(int neuronCount) {
-        this.neuronCount = neuronCount;
+    public static void main(String[] args) {
+        new EdgeDetectionRunner();
     }
 
+    @Override
+    public String toString() {
+        return "EdgeDetectionRunner";
+    }
 }
