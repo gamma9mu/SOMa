33b46c1da3483a3bab550391e286bc314ab4b6ef
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-14 13:47:23 +0800

    Clean up SOM2dPlotter.

--- src/cs437/som/visualization/SOM2dPlotter.java
+++ src/cs437/som/visualization/SOM2dPlotter.java
@@ -2,9 +2,8 @@ package cs437.som.visualization;
 
 import cs437.som.SelfOrganizingMap;
 
-import javax.swing.JFrame;
-import java.awt.Color;
-import java.awt.Graphics;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Visualization for SOM with a 2-dimensional input.
@@ -14,7 +13,9 @@ public class SOM2dPlotter extends JFrame {
     
     public static final int WIDTH = 400;
     public static final int HEIGHT = 400;
+
     SelfOrganizingMap map = null;
+    private int neuronCount = 0;
 
     /**
      * Create and setup a dot plot for a 2D input SOM.
@@ -26,6 +27,7 @@ public class SOM2dPlotter extends JFrame {
         if (map.getInputLength() != 2) throw new IllegalArgumentException("SOM does not map 2d inputs");
 
         this.map = map;
+        neuronCount = map.getNeuronCount();
         setSize(WIDTH, HEIGHT);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -39,33 +41,33 @@ public class SOM2dPlotter extends JFrame {
         Graphics g = getBufferStrategy().getDrawGraphics();
         g.setColor(Color.white);
         g.fillRect(0, 0, getWidth(), getHeight());
-
-        int neuronCount = map.getNeuronCount();
-        double[][] pts = new double[neuronCount][2];
-
-//        double xmin, xmax, ymin, ymax;
-//        xmin = xmax = pts[0][0] = map.getWeight(0, 0);
-//        ymin = ymax = pts[0][1] = map.getWeight(0, 1);
-
-        for (int i = 0; i < neuronCount; i++) {
-            pts[i][0] = map.getWeight(i, 0);
-            pts[i][1] = map.getWeight(i, 1);
-//            if (pts[i][0] < xmin) xmin = pts[i][0];
-//            if (pts[i][0] > xmax) xmax = pts[i][0];
-//            if (pts[i][1] < ymin) ymin = pts[i][1];
-//            if (pts[i][1] > ymax) ymax = pts[i][1];
-        }
-//        double dx = (xmax - xmin);
-//        double dy = (ymax - ymin);
-
         g.setColor(Color.black);
-        for (double[] pt : pts) {
-//            int x = (int) Math.round(pt[0] / dx * (getWidth() - 10));
-//            int y = (int) Math.round(pt[1] / dy * (getHeight() - 10));
-            int x = (int)Math.round(pt[0] * 30) + 20;
-            int y = (int)Math.round(pt[1] * 30) + 20;
+        
+        double[][] points = new double[neuronCount][2];
+
+        // find the x- and y-axis spread
+        double xmin = Double.MAX_VALUE, xmax = Double.MIN_VALUE;
+        double ymin = Double.MAX_VALUE, ymax = Double.MIN_VALUE;
+        for (int i = 0; i < neuronCount; i++) {
+            points[i][0] = map.getWeight(i, 0);
+            points[i][1] = map.getWeight(i, 1);
+            xmin = Math.min(xmin, points[i][0]);
+            ymin = Math.min(ymin, points[i][1]);
+            xmax = Math.max(xmax, points[i][0]);
+            ymax = Math.max(ymax, points[i][1]);
+        }
+
+        // compute the scaling factor
+        double dx = getWidth() / (xmax - xmin);
+        double dy = getHeight() / (ymax - ymin);
+
+        for (double[] pt : points) {
+            // zero-base the coordinates and scale them to window size
+            int x = (int) Math.round((pt[0] - xmin) * dx);
+            int y = (int) Math.round((pt[1] - ymin) * dy);
             g.drawOval(x - 2, y - 2, 4, 4);
         }
+
         getBufferStrategy().show();
     }
 
