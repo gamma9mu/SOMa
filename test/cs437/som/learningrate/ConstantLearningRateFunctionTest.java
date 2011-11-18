e3e54892445d408b00729d05420a4828cf3005d8
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-18 09:47:34 +0800

    Test: ConstantLearningRateFunctionTest.

---
+++ test/cs437/som/learningrate/ConstantLearningRateFunctionTest.java
@@ -0,0 +1,21 @@
+package cs437.som.learningrate;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class ConstantLearningRateFunctionTest {
+    private static final double MAXIMUM_DIFFERENCE = 0.0000000000000001;
+    private static final int ITERATIONS = 1000;
+    static final double LEARNING_RATE = 0.2;
+
+    @Test
+    public void testLearningRate() throws Exception {
+        ConstantLearningRateFunction clrf = new ConstantLearningRateFunction(LEARNING_RATE);
+        clrf.setExpectedIterations(ITERATIONS);
+        assertEquals(clrf.learningRate(1), LEARNING_RATE, MAXIMUM_DIFFERENCE);
+        assertEquals(clrf.learningRate(10), LEARNING_RATE, MAXIMUM_DIFFERENCE);
+        assertEquals(clrf.learningRate(100), LEARNING_RATE, MAXIMUM_DIFFERENCE);
+        assertEquals(clrf.learningRate(1000), LEARNING_RATE, MAXIMUM_DIFFERENCE);
+    }
+}
