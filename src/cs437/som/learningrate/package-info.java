030511cc29aebfe7f7960d301a7e13b4d252d94b
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-20 13:46:22 +0800

    Add a package annotation to learningrate.

---
+++ src/cs437/som/learningrate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The {@code learningrate} package contains strategy classes to parametrize
+ * self-organizing maps.  The classes are intended to be created by the user
+ * and given to a self-organizing map, which will assume control.  These
+ * classes all derive from {@link cs437.som.LearningRateFunction} in
+ * {@link cs437.som}.
+ */
+package cs437.som.learningrate;
