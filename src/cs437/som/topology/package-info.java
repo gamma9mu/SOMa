95e04fbd21f0af3719a7c8f9ca3e95d5bd251a29
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-20 00:01:43 -0600

    Add a package annotation to topology.

---
+++ src/cs437/som/topology/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The {@code topology} package contains neuron grid strategy classes for
+ * customizing self-organizing maps.  These classes are intended to be
+ * instantiated and immediately given to a SOM.  All classes in this package
+ * implement {@link cs437.som.GridType}.
+ */
+package cs437.som.topology;
