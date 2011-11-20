b0067771f602034f2ecaf8d3df2ddfa1e2a216a8
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-19 20:59:37 -0600

    Add ability to scale learning rate by the neighborhood membership.

---
+++ src/cs437/som/neighborhood/ContinuousUnitNormal.java
@@ -0,0 +1,8 @@
+package cs437.som.neighborhood;
+
+/**
+ * Marker interface denoting neighborhood width strategies that are based on
+ * continuous functions with a range in the interval [-1, 1].
+ */
+public interface ContinuousUnitNormal {
+}
