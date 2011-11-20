1b2883528c559368603715976a30df794b17ada4
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-19 23:56:01 -0600

    Add a package annotation to distancemetrics.

---
+++ src/cs437/som/distancemetrics/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The {@code distancemetrics} package contains strategy classes to parametrize
+ * the way self-organizing maps measure the distance from a neuron to an input
+ * vector.  The classes are intended to be created by the user and given to a
+ * self-organizing map, which will assume control.  These classes all derive
+ * from {@link cs437.som.DistanceMetric} in {@link cs437.som}.
+ */
+package cs437.som.distancemetrics;
