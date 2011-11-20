93403362091b90198801453abe18699edca10f66
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-20 00:12:51 -0600

    Add a package annotation to neighborhood.

---
+++ src/cs437/som/neighborhood/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The {@code neighborhood} package contains neighborhood sizing strategy
+ * classes to parametrize the way self-organizing maps measure the size of the
+ * neuron grid's neighborhood.  The classes are intended to be created by the
+ * user and given to a self-organizing map, which will assume control.  These
+ * classes all implement {@link cs437.som.NeighborhoodWidthFunction} in
+ * {@link cs437.som}.
+ */
+package cs437.som.neighborhood;
