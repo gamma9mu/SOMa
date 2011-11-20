d4b101f0e887dfeb6500eb0390c0e2c654a06223
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-20 14:16:18 +0800

    Add a package annotation to network.

---
+++ src/cs437/som/network/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The {@code network} package contains classes implementing self-organizing
+ * maps.  These classes all implement
+ * {@link cs437.som.TrainableSelfOrganizingMap} in {@link cs437.som}, either
+ * directly or indirectly.
+ */
+package cs437.som.network;
