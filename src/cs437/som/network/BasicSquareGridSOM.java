2ed4a30d235ea7e7d09cf359478ed29cee9fd258
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-22 09:20:12 +0800

    Clean up imports.

--- src/cs437/som/network/BasicSquareGridSOM.java
+++ src/cs437/som/network/BasicSquareGridSOM.java
@@ -1,14 +1,11 @@
 package cs437.som.network;
 
 import cs437.som.Dimension;
-import cs437.som.SOMError;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * A simple self-organizing map, using a square grid for the neurons.
