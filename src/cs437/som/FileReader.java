7eccae859cfd2ded33060a0a99c23b4fb1c90b50
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-22 14:08:20 +0800

    Remove testing main() from FileReader.

--- src/cs437/som/FileReader.java
+++ src/cs437/som/FileReader.java
@@ -53,10 +53,4 @@ public class FileReader {
     public String toString() {
         return "FileReader";
     }
-
-    public static void main(String[] args) throws IOException {
-        new FileReader(new File("custom.som")).tsom.write(
-                new FileWriter("custom.out.som")
-        );
-    }
 }
