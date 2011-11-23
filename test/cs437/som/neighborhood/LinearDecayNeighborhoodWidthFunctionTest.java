7e1fc48f2cd2401d4dc5bac844415dd158bfaef1
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-23 15:41:47 +0800

    Test: Split tests into seperate methods in LinearDecayNeighborhoodWidthFunctionTest.

--- test/cs437/som/neighborhood/LinearDecayNeighborhoodWidthFunctionTest.java
+++ test/cs437/som/neighborhood/LinearDecayNeighborhoodWidthFunctionTest.java
@@ -1,5 +1,6 @@
 package cs437.som.neighborhood;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -11,31 +12,41 @@ public class LinearDecayNeighborhoodWidthFunctionTest {
     private static final int ITERATIONS = 1000;
     private static final double WIDTH = 1.0;
 
-    @Test
-    public void testNeighborhoodWidth() throws Exception {
-        LinearDecayNeighborhoodWidthFunction ldnwf =
-                new LinearDecayNeighborhoodWidthFunction(WIDTH);
-        ldnwf.setExpectedIterations(ITERATIONS);
+    private LinearDecayNeighborhoodWidthFunction ldnwf;
 
+    @BeforeTest
+    public void setUp() {
+        ldnwf = new LinearDecayNeighborhoodWidthFunction(WIDTH);
+        ldnwf.setExpectedIterations(ITERATIONS);
+    }
+
+    @Test
+    public void testAlwaysDecays() throws Exception {
         double last = ldnwf.neighborhoodWidth(0);
         for (int i = 1; i < ITERATIONS; i++) {
             double current = ldnwf.neighborhoodWidth(i);
             assertTrue(current < last, "Should consistently decay.");
             last = current;
         }
+    }
 
-        double slope = ldnwf.neighborhoodWidth(1) - ldnwf.neighborhoodWidth(0);
-        for (int i = 1; i < ITERATIONS; i++) {
-            double currentSlope = ldnwf.neighborhoodWidth(i+1)
-                    - ldnwf.neighborhoodWidth(i);
-            assertEquals(currentSlope, slope, MAXIMUM_SLOPE_DIFFERENCE,
-                    String.format("Should have constant slope. @%d", i));
-        }
-
+    @Test
+    public void testMustBeLinear() {
         assertEquals(ldnwf.neighborhoodWidth(0), WIDTH, MAXIMUM_DIFFERENCE);
         for (int i = 1; i < ITERATIONS; i++) {
             assertEquals(ldnwf.neighborhoodWidth(i), WIDTH - ((double) i / ITERATIONS),
                     MAXIMUM_DIFFERENCE, "Should be exactly linear.");
+        }
+    }
+
+    @Test
+    public void testConstantSlope() {
+        double slope = ldnwf.neighborhoodWidth(1) - ldnwf.neighborhoodWidth(0);
+        for (int i = 1; i < ITERATIONS; i++) {
+            double currentSlope = ldnwf.neighborhoodWidth(i + 1)
+                    - ldnwf.neighborhoodWidth(i);
+            assertEquals(currentSlope, slope, MAXIMUM_SLOPE_DIFFERENCE,
+                    String.format("Should have constant slope. @%d", i));
         }
     }
 }
