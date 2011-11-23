4e828a1f3fa03ef4ae5d73da860cd6ddbb13129b
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-23 01:56:05 -0600

    Rename gui package to util.

--- src/cs437/som/gui/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * The {@code gui} package contains classes meant to aid in experimentation
- * with self-organizing maps for a given application.  
- */
-package cs437.som.gui;
---
+++ src/cs437/som/util/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The {@code util} package contains classes meant to aid in experimentation
+ * with self-organizing maps for a given application.
+ */
+package cs437.som.util;
