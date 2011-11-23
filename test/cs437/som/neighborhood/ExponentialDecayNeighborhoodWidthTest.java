2f1b42c937c792e38c1534c8dcafe5622c27cb28
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-23 15:48:15 +0800

    Test: Split tests into seperate methods in ExponentialDecayNeighborhoodWidthTest.

--- test/cs437/som/neighborhood/ExponentialDecayNeighborhoodWidthTest.java
+++ test/cs437/som/neighborhood/ExponentialDecayNeighborhoodWidthTest.java
@@ -1,5 +1,6 @@
 package cs437.som.neighborhood;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -10,28 +11,38 @@ public class ExponentialDecayNeighborhoodWidthTest {
     private static final int ITERATIONS = 1000;
     private static final double WIDTH = 1.0;
 
-    @Test
-    public void testNeighborhoodWidth() throws Exception {
-        ExponentialDecayNeighborhoodWidth ednwf =
-                new ExponentialDecayNeighborhoodWidth(WIDTH);
-        ednwf.setExpectedIterations(ITERATIONS);
+    private ExponentialDecayNeighborhoodWidth ednwf;
 
+    @BeforeTest
+    public void setUp() {
+        ednwf = new ExponentialDecayNeighborhoodWidth(WIDTH);
+        ednwf.setExpectedIterations(ITERATIONS);
+    }
+
+    @Test
+    public void testConsistentDecay() throws Exception {
         double last = ednwf.neighborhoodWidth(1);
         for (int i = 2; i < ITERATIONS; i += 10) {
             double current = ednwf.neighborhoodWidth(i);
             assertTrue(current < last, "Should consistently decay.");
             last = current;
         }
+    }
 
+    @Test
+    public void testByDirectComparison() {
+        for (int i = 0; i < ITERATIONS; i++) {
+            assertEquals(ednwf.neighborhoodWidth(i),
+                    Math.exp(-(double)i / ITERATIONS), MAXIMUM_DIFFERENCE);
+        }
+    }
+
+    @Test
+    public void testLessThanLinear() {
         assertEquals(ednwf.neighborhoodWidth(0), WIDTH, MAXIMUM_DIFFERENCE);
         for (int i = 1; i < ITERATIONS; i++) {
             assertTrue(ednwf.neighborhoodWidth(i) < WIDTH - (i / ITERATIONS),
                     "Exponential decay should be less than linear.");
-        }
-
-        for (int i = 0; i < ITERATIONS; i++) {
-            assertEquals(ednwf.neighborhoodWidth(i), Math.exp(-(double)i / ITERATIONS),
-                    MAXIMUM_DIFFERENCE, String.format("it: %d", i));
         }
     }
 }
