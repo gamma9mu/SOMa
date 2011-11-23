3896847e2fffb2d8528abfcb8b6d87ad50b231e0
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-23 01:43:38 -0600

    Test: Split tests into seperate methods in HyperbolicNeighborhoodWidthFunctionTest.

--- test/cs437/som/neighborhood/HyperbolicNeighborhoodWidthFunctionTest.java
+++ test/cs437/som/neighborhood/HyperbolicNeighborhoodWidthFunctionTest.java
@@ -1,5 +1,6 @@
 package cs437.som.neighborhood;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -17,12 +18,16 @@ public class HyperbolicNeighborhoodWidthFunctionTest {
     private static final double[] SAMPLE_ACCURACY =
             {1.0e-10, 1.0e-5, 1.0e-5, 1.0e-5, 1.0e-10};
 
-    @Test
-    public void testNeighborhoodWidth() throws Exception {
-        HyperbolicNeighborhoodWidthFunction hnwf =
-                new HyperbolicNeighborhoodWidthFunction(START_WIDTH, END_WIDTH);
-        hnwf.setExpectedIterations(ITERATIONS);
+    private HyperbolicNeighborhoodWidthFunction hnwf;
 
+    @BeforeTest
+    public void setUp() {
+        hnwf = new HyperbolicNeighborhoodWidthFunction(START_WIDTH, END_WIDTH);
+        hnwf.setExpectedIterations(ITERATIONS);
+    }
+
+    @Test
+    public void testConsistentDecay() throws Exception {
         double last = hnwf.neighborhoodWidth(1);
         for (int i = 2; i < ITERATIONS; i += 10) {
             double current = hnwf.neighborhoodWidth(i);
@@ -30,16 +35,23 @@ public class HyperbolicNeighborhoodWidthFunctionTest {
             last = current;
         }
 
+        testBySampling();
+    }
+
+    @Test
+    public void testBySampling() {
+        for (int j = 0; j < SAMPLES.length; j++) {
+            assertEquals(hnwf.neighborhoodWidth(SAMPLES[j]), SAMPLE_RESULTS[j],
+                    SAMPLE_ACCURACY[j]);
+        }
+    }
+
+    @Test
+    public void testLessThanLinear() {
         assertEquals(hnwf.neighborhoodWidth(0), START_WIDTH, MAXIMUM_DIFFERENCE);
         for (int i = 1; i < ITERATIONS; i++) {
             assertTrue(hnwf.neighborhoodWidth(i) < START_WIDTH - (i / ITERATIONS),
                     "Hyperbolic function should be less than linear.");
-        }
-
-        // Verify the function fits known samples
-        for (int j = 0; j < SAMPLES.length; j++) {
-            assertEquals(hnwf.neighborhoodWidth(SAMPLES[j]), SAMPLE_RESULTS[j],
-                    SAMPLE_ACCURACY[j]);
         }
     }
 }
