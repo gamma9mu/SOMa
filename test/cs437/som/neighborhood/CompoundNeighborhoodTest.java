9870cad73fb76ef345014c11d1696920324353df
Brian Guthrie <baguthrie@sbcglobal.net>
2011-11-23 01:45:39 -0600

    Test: Split tests into seperate methods in CompoundNeighborhoodTest.

--- test/cs437/som/neighborhood/CompoundNeighborhoodTest.java
+++ test/cs437/som/neighborhood/CompoundNeighborhoodTest.java
@@ -1,5 +1,6 @@
 package cs437.som.neighborhood;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -10,22 +11,30 @@ public class CompoundNeighborhoodTest {
     private static final int FIRST_VALUE = 10;
     private static final int SECOND_VALUE = 2;
 
-    @Test
-    public void testNeighborhoodWidth() throws Exception {
-        ConstantNeighborhoodWidthFunction cnwf0 =
-                new ConstantNeighborhoodWidthFunction(FIRST_VALUE);
-        cnwf0.setExpectedIterations(CROSSOVER_POINT);
-        ConstantNeighborhoodWidthFunction cnwf1 =
-                new ConstantNeighborhoodWidthFunction(SECOND_VALUE);
-        cnwf1.setExpectedIterations(CROSSOVER_POINT);
-        CompoundNeighborhood cn = new CompoundNeighborhood(cnwf0);
-        cn.addNeighborhood(cnwf1, CROSSOVER_POINT);
+    private ConstantNeighborhoodWidthFunction cnwf0;
+    private ConstantNeighborhoodWidthFunction cnwf1;
+    private CompoundNeighborhood cn;
 
+    @BeforeTest
+    public void setUp() {
+        cnwf0 = new ConstantNeighborhoodWidthFunction(FIRST_VALUE);
+        cnwf0.setExpectedIterations(CROSSOVER_POINT);
+        cnwf1 = new ConstantNeighborhoodWidthFunction(SECOND_VALUE);
+        cnwf1.setExpectedIterations(CROSSOVER_POINT);
+        cn = new CompoundNeighborhood(cnwf0);
+        cn.addNeighborhood(cnwf1, CROSSOVER_POINT);
+    }
+
+    @Test
+    public void testFirstChild() throws Exception {
         for (int i = 0; i < CROSSOVER_POINT; i++) {
             assertEquals(cn.neighborhoodWidth(i), FIRST_VALUE,
                     MAXIMUM_DIFFERENCE);
         }
+    }
 
+    @Test
+    public void testSecondChild() {
         for (int j = 0; j < CROSSOVER_POINT; j++) {
             assertEquals(cn.neighborhoodWidth(j + CROSSOVER_POINT),
                     FIRST_VALUE, MAXIMUM_DIFFERENCE);
