094d25968f8e8d794222320f594e0e98dc8ce88e
Brian Guthrie <baguthrie@sbcglobal.net>
2011-12-08 20:42:03 -0600

    Add @Override to ExponentialNeighborhoodMembershipFunction.

--- src/cs437/som/membership/ExponentialNeighborhoodMembershipFunction.java
+++ src/cs437/som/membership/ExponentialNeighborhoodMembershipFunction.java
@@ -3,10 +3,10 @@ package cs437.som.membership;
 import cs437.som.NeighborhoodMembershipFunction;
 
 public class ExponentialNeighborhoodMembershipFunction implements NeighborhoodMembershipFunction {
+    @Override
     public double neighborhoodMembership(double distance, double width) {
-        if (distance < width) {
+        if (distance < width)
             return Math.exp(-(width - distance) / width);
-        }
         else
             return 0;
     }
