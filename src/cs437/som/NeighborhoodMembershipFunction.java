a75f5cf423555f50b34917d18fc4040402ec2255
Brian Guthrie <baguthrie@sbcglobal.net>
2011-12-08 21:47:09 -0600

    Add missing javadoc in NeighborhoodMembershipFunction.

--- src/cs437/som/NeighborhoodMembershipFunction.java
+++ src/cs437/som/NeighborhoodMembershipFunction.java
@@ -4,5 +4,13 @@ package cs437.som;
  * Neighborhood membership strategy interface for self-organizing maps.
  */
 public interface NeighborhoodMembershipFunction {
+
+    /**
+     * Compute a neuron's membership amount in the BMU's network.
+     *
+     * @param distance The distance from the neuron in question to the BMU.
+     * @param width The current width of the BMU's neighborhood.
+     * @return The membership rate of the neuron in the BMU's neighborhood.
+     */
     double neighborhoodMembership(double distance, double width);
 }
