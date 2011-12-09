aab53db1081765d6fa0a878d5150884ef4e2d32b
Brian Guthrie <baguthrie@sbcglobal.net>
2011-12-08 21:09:07 -0600

    Document ExponentialNeighborhoodMembershipFunction.

--- src/cs437/som/membership/ExponentialNeighborhoodMembershipFunction.java
+++ src/cs437/som/membership/ExponentialNeighborhoodMembershipFunction.java
@@ -2,6 +2,17 @@ package cs437.som.membership;
 
 import cs437.som.NeighborhoodMembershipFunction;
 
+/**
+ * Exponential neighborhood membership strategy.
+ *
+ * This membership function follows the equation:
+ * <pre>
+ *     e^{-\frac{w - \delta}{\w}}
+ * where
+ *     w       is the neighborhood width
+ *     \delta  is the distance from the neuron in question to the BMU
+ * </pre>
+ */
 public class ExponentialNeighborhoodMembershipFunction implements NeighborhoodMembershipFunction {
     @Override
     public double neighborhoodMembership(double distance, double width) {
