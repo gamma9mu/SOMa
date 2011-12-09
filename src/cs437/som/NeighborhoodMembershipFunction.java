4011eab44d6986bf7215ee6cffd30123fb29ec6e
Brian Guthrie <baguthrie@sbcglobal.net>
2011-12-08 21:35:48 -0600

    Document NeighborhoodMembershipFunction.

--- src/cs437/som/NeighborhoodMembershipFunction.java
+++ src/cs437/som/NeighborhoodMembershipFunction.java
@@ -1,5 +1,8 @@
 package cs437.som;
 
+/**
+ * Neighborhood membership strategy interface for self-organizing maps.
+ */
 public interface NeighborhoodMembershipFunction {
     double neighborhoodMembership(double distance, double width);
 }
